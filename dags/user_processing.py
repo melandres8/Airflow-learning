b3d8c17cd747fb01d249803b5438e6bfa6cd0e13
Melkin Mosquera <melanmosta@gmail.com>
2022-04-25 19:56:44 -0500

    adding storing_user task and deleting useless file

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pandas import json_normalize
 
 from airflow.models import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
@@ -67,4 +68,9 @@ with DAG('user_processing', schedule_interval='@daily',
     processing_user = PythonOperator(
         task_id='processing_user',
         python_callable=_processing_user
+    )
+
+    storing_user = BashOperator(
+        task_id='storing_user',
+        bash_command='echo -e ".separator ","\n.import /tmp/processed_user.csv users" | sqlite3 /Users/melandres/airflow/airflow.db'
     )
