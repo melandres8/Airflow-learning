be3ed76164a33f2fa07236a2c587f479284a112a
Melkin Mosquera <melanmosta@gmail.com>
2022-04-25 19:32:53 -0500

    adding extracting_user and processing_user tasks to the pipeline

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,12 +1,33 @@
+import json
+from datetime import datetime
+from pandas import json_normalize
+
 from airflow.models import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 
-from datetime import datetime
 
 default_args = {
     'start_date': datetime(2022, 4, 24)
 }
+
+def _processing_user(ti):
+    users = ti.xcom_pull(task_ids='extracting_user')
+    if not len(users) or 'results' not in users:
+        raise ValueError('User is empty')
+    user = users['results'][0]
+    processed_user = json_normalize({
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email']
+    })
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+
 
 with DAG('user_processing', schedule_interval='@daily',
     default_args=default_args,
@@ -32,4 +53,18 @@ with DAG('user_processing', schedule_interval='@daily',
         task_id='is_api_available',
         http_conn_id='user_api',
         endpoint='api/'
+    )
+
+    extracting_user = SimpleHttpOperator(
+        task_id='extracting_user',
+        http_conn_id='user_api',
+        endpoint='api/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+
+    processing_user = PythonOperator(
+        task_id='processing_user',
+        python_callable=_processing_user
     )
